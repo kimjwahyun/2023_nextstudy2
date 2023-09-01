8ea2ddba8df7430671fb26adc7463f60f4bdb627
kimjwahyun <whkgus94@naver.com>
2023-09-01 13:17:30 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode:true,
+    swcMinify:true,
+    async rewrites(){
+        return[{
+            source :"/api/:path*",
+            destination : "http://makeup-api.herokuapp.com/api/:path*"
+        }];
+    }
+}
+
+module.exports = nextConfig
